43fe3b8dae2ad43bd3603b35f0a6a01b1a8225a7
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-07-11 04:13:36 +0500

    [core] removed unintentional commas

--- core/enums.py
+++ core/enums.py
@@ -8,22 +8,22 @@ class ConsoleTheme(Enum):
     info    = ''
 
 class Emoji:
-    error    = "<:error:856131306739335198>",
-    warning  = "<:warning:856131306777608202>",
-    info     = ":information_source:",
+    error    = "<:error:856131306739335198>"
+    warning  = "<:warning:856131306777608202>"
+    info     = ":information_source:"
     lookup   = ":face_with_monocle:"
-    neutral  = "<:neutral:856131307176067076>",
-    success  = "<:success:856131306415718420>",
-    settings = ":gear:",
-    enabled  = ":green_circle:",
-    disabled = ":red_circle:",
+    neutral  = "<:neutral:856131307176067076>"
+    success  = "<:success:856131306415718420>"
+    settings = ":gear:"
+    enabled  = ":green_circle:"
+    disabled = ":red_circle:"
     channel  = ":hash:"
 
 class Color:
-    primary   = 10052333,
-    secondary = 8232447,
-    error     = 15417396,
-    warning   = 15763255,
-    info      = 4879080,
-    neutral   = 3092790,
+    primary   = 10052333
+    secondary = 8232447
+    error     = 15417396
+    warning   = 15763255
+    info      = 4879080
+    neutral   = 3092790
     success   = 5038123
