45491a039ee020bd31e8003c5ddd39fe40b46e17
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-07-14 02:16:25 +0500

    Add basic functionality of the removal of data on guild remove

---
+++ cogs/guild_listeners.py
@@ -0,0 +1,40 @@
+import discord
+from discord.ext import commands
+
+class GuildListeners(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        await self.bot.mongo['guilds_config']['behaviour'].insert_one({
+            '_id': guild.id,
+            'prefix': 'sly ',
+            'track_deletes': 1,
+            'track_edits': 1,
+            'easters': 0
+            })
+        embed = discord.Embed(title=":wave: Hi peeps!", description="I'm Sly and I'm here to make day of each and everyone here, I hope ya'll like me and I'll try my best to entertain ya'll.")
+        embed.add_field(name="Help Command", value="My prefix is `sly`, you can use `sly help` to see all commands and categories! :ok_hand:", inline=False)
+        embed.add_field(name="Some links you might wanna check out", value="- [Docs](https://slybot.gitbook.io)\n- [GitHub](https://github.com/nerdguyahmad/sly)\n- [Issue Tracker](https://github.com/nerdguyahmad/sly)\n- [Website](https://sly-bot.web.app)\n- [Status](https://sly-bot.web.app/status.html)\n- [Support & Community server](https://sly-bot.web.app/discord.html)\n- [Invite](https://sly-bot.web.app/invite.html)", inline=False)
+        embed.color = discord.Color.random()
+
+        for i in guild.channels:
+            if isinstance(i, discord.TextChannel):
+                if i.permissions_for(guild.me).send_messages:
+                    await ctx.send(embed=embed)
+                    break
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        await self.bot.mongo['guilds_config']['behaviour'].delete_one({
+            '_id': guild.id
+            })
+        await self.bot.mongo['guilds_config']['confessions'].delete_one({
+            '_id': guild.id
+            })
+        
+
+    
+def setup(bot):
+    bot.add_cog(GuildListeners(bot))
