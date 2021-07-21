78af06931a6c806b84fa810bc9ccc86ce8394c5d
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-07-21 06:11:06 +0500

    Add variable resolver function

--- cogs/admin.py
+++ cogs/admin.py
@@ -5,5 +5,18 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    def resolve_variable(self, variable):
+        if hasattr(variable, "__iter__"):
+            var_length = len(list(variable))
+            if (var_length > 100) and (not isinstance(variable, str)):
+                return f"<a {type(variable).__name__} iterable with more than 100 values ({var_length})>"
+            elif (not var_length):
+                return f"<an empty {type(variable).__name__} iterable>"
+        
+        if (not variable) and (not isinstance(variable, bool)):
+            return f"<an empty {type(variable).__name__} object>"
+        return (variable if (len(f"{variable}") <= 1000) else f"<a long {type(variable).__name__} object with the length of {len(f'{variable}'):,}>")
+    
+
 def setup(bot):
     bot.add_cog(Admin(bot))
